1622f2ee1068dfff167cecddbc7d6e8d3ab2379d
Justin Rockwood <justinrockwood@hotmail.com>
2017-10-09 01:34:18 -0700

    feat: rockos v0.1
    Displays a copyright banner using a hand-assembled binary bootloader.

---
+++ src/rockos/v0.1/build.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+pushd .
+cd %~dp0..\..\..\
+
+echo Creating a blank floppy disk image...
+node tools\rockdisk\bin\rockdisk create --out disks\rockos.vfd --type floppy
+
+echo Copying the bootloader to the floppy disk image...
+node tools\rockdisk\bin\rockdisk copy --src %~dp0bootload.bin --dest disks\rockos.vfd --doff 0
+
+popd
+echo Done
