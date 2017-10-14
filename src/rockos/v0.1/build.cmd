d084542aee524bf8946459ddd54c6baccd44a7bd
Justin Rockwood <justinrockwood@hotmail.com>
2017-10-14 10:41:31 -0700

    chore: add quotes around args in build.cmd

--- src/rockos/v0.1/build.cmd
+++ src/rockos/v0.1/build.cmd
@@ -7,7 +7,7 @@ echo Creating a blank floppy disk image...
 node tools\rockdisk\bin\rockdisk create --out disks\rockos.vfd --type floppy
 
 echo Copying the bootloader to the floppy disk image...
-node tools\rockdisk\bin\rockdisk copy --src %~dp0bootload.bin --dest disks\rockos.vfd --doff 0
+node tools\rockdisk\bin\rockdisk copy --src "%~dp0bootload.bin" --dest disks\rockos.vfd --doff 0
 
 popd
 echo Done
