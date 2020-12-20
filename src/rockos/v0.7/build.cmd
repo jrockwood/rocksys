79bf12d157207aae51bd79c6683a3c720f541434
Justin Rockwood <justinrockwood@hotmail.com>
2020-12-20 12:23:42 -0800

    Create build.cmd for the OS and fix build errors

---
+++ src/rockos/v0.7/build.cmd
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+
+pushd .
+cd "%~dp0..\..\.."
+
+echo Building RockOS v0.7
+echo ====================
+echo.
+node tools\rockdisk\bin\rockdisk compile-os --destVfd disks\rockos.vfd --srcDir src --asmVersion v0.3 --osVersion v0.7 --sectorMap "%~dp0sectorMap.json"
+
+echo Done!
+
+popd
+endlocal
